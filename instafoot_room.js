bc05557dbd1c4d568b26afb7269e5d8ce5ebf8d3
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-06-10 10:46:50 -0700

    Add files via upload

---
+++ instafoot_room.js
@@ -0,0 +1,12 @@
+username = localStorage.getItem("username")
+document.getElementById('username').innerHTML = "Welcome to InstaFoot " + username + "!";
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
